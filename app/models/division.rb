ed0f3a2af4084f96f3eba26dfe7837957684ab90
saasbook <saasbook@saasbook.(none)>
2013-11-24 19:15:34 -0800

    adding to division view

--- app/models/division.rb
+++ app/models/division.rb
@@ -22,7 +22,7 @@ class Division < ActiveRecord::Base
        self.teams << fake_team
     end
     for i in 1...self.teams.length
-      for j in i...fake_teams.length
+      for j in i...self.teams.length
         test_game = Game.create!
         test_game.team1_id = self.teams.at(i).id
         test_game.team2_id = self.teams.at(j).id
