64d85bf4eefdd69fa03e36d66701648157019e4f
Francis Wang <fxwang@berkeley.edu>
2013-12-09 13:41:19 -0800

    Drop down notifications bar shows both notifications and invites

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,12 +2,6 @@ class Notification < Message
   belongs_to :game
   belongs_to :user
   belongs_to :team
-  attr_accessible :game, :user, :team, :note
-  after_create :iterate_user_counter
+  attr_accessible :game, :user, :team
 
-  def iterate_user_counter
-    if (self.user)
-      self.user.iterate_notification_counter
-    end
-  end
 end
