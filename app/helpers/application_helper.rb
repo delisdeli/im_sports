652740a1042f6d1952a4692abc07eb499a00414c
Francis Wang <fxwang@berkeley.edu>
2013-12-09 15:13:55 -0800

    Move notification/invitation stuff to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def message_bar_helper(msg, index)
+    return case msg.type
+      when "Notification" then link_to "#{index+1}: The schedule for your team #{msg.team.name} has changed.", [msg.game.division.league, msg.game.division, msg.game]
+      when "Invitation" then link_to "#{index+1}: You have been invited to join team #{msg.team.name}.", current_user
+    end
+  end
 end
