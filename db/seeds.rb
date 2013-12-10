3752b5954a1115c800f297507f902152b0764256
Andrew Wang <andrewkw@berkeley.edu>
2013-12-10 02:48:13 -0800

    seed data now properly shows up in roster

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,22 +27,41 @@ Division.create(end_time:'8PM', game_length:60, name:'Division 6', num_teams:8, 
 Division.create(end_time:'8PM', game_length:60, name:'Division 7', num_teams:8, start_time:'6PM', num_locations:2, league_id:7, start_date:Date.new(2013, 8, 29), num_weeks:7)
 Division.create(end_time:'8PM', game_length:60, name:'Division 8', num_teams:8, start_time:'6PM', num_locations:2, league_id:8, start_date:Date.new(2013, 8, 23), num_weeks:7)
 
-Team.create(captain_email:'dennis@berkeley.edu', name:'Wildcats', division_id:1, placeholder:false)
-Team.create(captain_email:'francis@berkeley.edu', name:'Bears', division_id:2, placeholder:false)
-Team.create(captain_email:'andrew@berkeley.edu', name:'Wolves', division_id:3, placeholder:false)
-Team.create(captain_email:'diana@berkeley.edu', name:'Baby Bears', division_id:4, placeholder:false)
-Team.create(captain_email:'daphne@berkeley.edu', name:'Alpha Beta Gamma', division_id:5, placeholder:false)
-Team.create(captain_email:'yuuki@berkeley.edu', name:'Giraffes', division_id:6, placeholder:false)
-Team.create(captain_email:'daniel@berkeley.edu', name:'AAA', division_id:7, placeholder:false)
-Team.create(captain_email:'sam@berkeley.edu', name:'Theta Tau', division_id:8, placeholder:false)
-Team.create(captain_email:'anni@berkeley.edu', name:'Cal Bears', division_id:1, placeholder:false)
+team1 = Team.create(captain_email:'dennis@berkeley.edu', name:'Wildcats', division_id:1, placeholder:false)
+team2 = Team.create(captain_email:'francis@berkeley.edu', name:'Bears', division_id:2, placeholder:false)
+team3 = Team.create(captain_email:'andrew@berkeley.edu', name:'Wolves', division_id:3, placeholder:false)
+team4 = Team.create(captain_email:'diana@berkeley.edu', name:'Baby Bears', division_id:4, placeholder:false)
+team5 = Team.create(captain_email:'daphne@berkeley.edu', name:'Alpha Beta Gamma', division_id:5, placeholder:false)
+team6 = Team.create(captain_email:'yuuki@berkeley.edu', name:'Giraffes', division_id:6, placeholder:false)
+team7 = Team.create(captain_email:'daniel@berkeley.edu', name:'AAA', division_id:7, placeholder:false)
+team8 = Team.create(captain_email:'sam@berkeley.edu', name:'Theta Tau', division_id:8, placeholder:false)
+team9 = Team.create(captain_email:'anni@berkeley.edu', name:'Cal Bears', division_id:1, placeholder:false)
 
-User.create(name:'Dennis', email:'dennis@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
-User.create(name:'Francis', email:'francis@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
-User.create(name:'Daphne', email:'daphne@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
-User.create(name:'Diana', email:'diana@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
-User.create(name:'Andrew', email:'Andrew@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
-User.create(name:'Yuuki', email:'Yuuki@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
-User.create(name:'Sam', email:'Sam@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
-User.create(name:'Anni', email:'Anni@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
+user1 = User.create(name:'Dennis', email:'dennis@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
+user2 = User.create(name:'Francis', email:'francis@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
+user3 = User.create(name:'Andrew', email:'andrew@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
+user4 = User.create(name:'Daphne', email:'daphne@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
+user5 = User.create(name:'Diana', email:'diana@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
+user6 = User.create(name:'Yuuki', email:'yuuki@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
+user7 = User.create(name:'Daniel', email:'daniel@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
+user8 = User.create(name:'Sam', email:'sam@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
+user9 = User.create(name:'Anni', email:'anni@berkeley.edu', password:'123456', password_confirmation:'123456', remember_token:1)
 
+team1.users << user1
+team2.users << user2
+team3.users << user3
+team4.users << user4
+team5.users << user5
+team6.users << user6
+team7.users << user7
+team8.users << user8
+team9.users << user9
+team1.save
+team2.save
+team3.save
+team4.save
+team5.save
+team6.save
+team7.save
+team8.save
+team9.save
